3818b207191260abe4e4334773e569acceda99bf
c0decaps <manuel.weiss@protonmail.ch>
2022-03-09 02:36:04 +0100

    initial commit with basic functionality

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<center>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+ <input type="file" name="uploaded" class="file-upload">
+ <input type="submit" value="print!" class="submit">
+</form>
+</body>
+</html>
---
+++ upload.php
@@ -0,0 +1,40 @@
+
+<?php
+$target_dir = "/var/www/html/uploads/";
+$target_file = $target_dir . basename($_FILES["uploaded"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$debug = false;
+
+// is the file an actual image?
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["uploaded"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["uploaded"]["tmp_name"], $target_file)) {
+	  echo $target_file." has been uploaded<br>";
+	  #$cmd = "bash /home/pi/print_img.sh ".$target_file;
+	  $cmd = "bash print_img.sh ".$target_file;
+	  shell_exec($cmd);
+	  echo "ran command: ".$cmd."<br>";
+	  if($debug == false) {
+	  	header('Location: http://'.$_SERVER['HTTP_HOST']);
+	  }
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+?>
+
