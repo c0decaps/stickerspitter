ba5006d57123b9b2181295732e7f423232510f99
c0decaps <manuel.weiss@protonmail.ch>
2022-11-22 02:56:31 +0100

    first style changes

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel='stylesheet' href='style.css'>
+<title>Stickerprinter</title>
 </head>
 <body>
 <center>
-<form action="upload.php" method="post" enctype="multipart/form-data">
- <input type="file" name="uploaded" class="file-upload">
+<div class='sticker-bg'>
+<div class='fileupload'>
+<form class = 'uploadform' action="upload.php" method="post" enctype="multipart/form-data">
+ <input type="file" name="uploaded" class="file-upload"><br>
  <input type="submit" value="print!" class="submit">
 </form>
+</div>
+</div>
 </body>
 </html>
